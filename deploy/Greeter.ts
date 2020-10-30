685e0e589a5ee564c0f0cc126c2583a795d88ec5
Robin Pedersen <robin@roberto.local>
2020-10-30 13:12:45 +0100

    Updated hardhat-deploy

--- deploy/Greeter.ts
+++ deploy/Greeter.ts
@@ -1,12 +1,16 @@
-import { HardhatRuntimeEnvironment, DeployFunction } from "hardhat/types";
+module.exports = async ({
+  getNamedAccounts,
+  deployments,
+  getChainId,
+  getUnnamedAccounts,
+}) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
 
-const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const { deploy } = hre.deployments;
-  const { deployer } = await hre.getNamedAccounts();
+  // the following will only deploy "GenericMetaTxProcessor" if the contract was never deployed or if the code changed since last deployment
   await deploy("Greeter", {
     from: deployer,
     // gas: 4000000,
     args: ["Greeting set from ./deploy/Greeter.ts"],
   });
 };
-export default func;
